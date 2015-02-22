be2b1335693952d4314d08297e7af6196a67e4b3
gaofanfan <1125774809@qq.com>
2015-02-22 13:51:35 +0800

    change item-test describe

--- __tests__/items-test.js
+++ __tests__/items-test.js
@@ -1,7 +1,7 @@
 jest.dontMock('../js/items');
 
 describe('.all()', function () {
-  it('should return array', function () {
+  it('should return correct price', function () {
     var Item = require('../js/items');
 
     var result = Item.all()[0];
