ac5115f6017a4071bd2ce9883987f8779b114571
gaofanfan <1125774809@qq.com>
2015-02-25 15:20:59 +0800

    add inventory.js file

---
+++ js/model/inventory.js
@@ -0,0 +1,18 @@
+var moment = require('moment');
+function Inventory(cart) {
+  this.cart = cart;
+}
+
+Inventory.prototype.toString = function(strategy) {
+  return '***<没钱赚商店>购物清单***\n' +
+    '打印时间：' + moment().format('YYYY年MM月DD日 HH:mm:ss') +
+    '\n\n----------------------\n' +
+    this.cart.getCartItemsText() +
+    '\n----------------------\n' + '优惠信息：\n' +
+    this.cart.getPromotionText(strategy) +
+    '\n----------------------\n' +
+    this.cart.getSummaryText(strategy) +
+    '**********************\n';
+};
+
+module.exports = Inventory;
