1c5487399207adeb353daf13e70267cc48dec022
gaofanfan <1125774809@qq.com>
2015-03-04 14:38:19 +0800

    add a promotion's brand-discount file

--- js/promotion/brand-discount.js
+++ js/promotion/brand-discount.js
@@ -1,4 +1,12 @@
 var Discount = require('./discount');
-function (argument) {
-  // body...
+function BrandDiscount(discount, subtotal, brand) {
+  Discount.call(this,discount, subtotal);
+  this.brand = brand;
 }
+
+BrandDiscount.prototype = Object.create(Discount.prototype);
+BrandDiscount.prototype.constructor = BrandDiscount;
+
+BrandDiscount.prototype.buildPromotionName = function () {
+  return this.brand + '品牌打折';
+};
